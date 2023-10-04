fa9a541b812723b20ef45ac789ac0bf8493b7bfe
giirafe <seojune408@gmail.com>
2023-10-04 18:47:05 +0900

    arbitrary input set

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from typing import List
 
-# Get user input as a list of integers
-nums_input = input("Enter nums list separated by spaces: ")
-nums = [int(x) for x in nums_input.split()]
+# nums_input = input("Enter nums list separated by spaces: ")
+# nums = [int(x) for x in nums_input.split()]
+# target = int(input("Enter target number: "))
 
-# Get user input for the target number as an integer
-target = int(input("Enter target number: "))
+nums = [3,2,4]
+target = 6
 
 # Define the twoSum function
 def twoSum(nums: List[int], target: int) -> List[int]:
-    # Your twoSum logic here
     result = []
-    # Example logic to find two numbers that sum to the target
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 result = [i, j]
                 break
+
     return result
 
-# Call the twoSum function with user input
 result = twoSum(nums, target)
 
 # Print the result
